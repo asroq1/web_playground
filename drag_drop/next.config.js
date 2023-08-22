854be0fb75d6a08e4bc08edd2a78bdce14a3604c
Devicii <62472550+asroq1@users.noreply.github.com>
2023-08-22 17:34:41 +0900

    update : last commit

--- drag_drop/next.config.js
+++ drag_drop/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    domains: ['source.unsplash.com'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+  },
 }
 
 module.exports = nextConfig
--- drag_drop/src/app/page.tsx
+++ drag_drop/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Image from 'next/image'
 import Box from '@mui/material/Box'
 import { useEffect, useState } from 'react'
 import 'react-resizable/css/styles.css' // 리사이징 기능을 위한 css 추가
@@ -11,7 +11,7 @@ import { log } from 'console'
 const ResponsiveGridLayout = WidthProvider(Responsive)
 
 export default function App() {
-  const layouts = [
+  const [layouts, setLayout] = useState([
     { i: '1', x: 0, y: 0, w: 1, h: 1, name: '1' },
     { i: '2', x: 1, y: 0, w: 1, h: 1 },
     { i: '3', x: 2, y: 0, w: 1, h: 1 },
@@ -20,69 +20,119 @@ export default function App() {
     { i: '6', x: 1, y: 1, w: 1, h: 1 },
     { i: '7', x: 2, y: 1, w: 1, h: 1, name: '7' },
     { i: '8', x: 3, y: 1, w: 1, h: 1 },
-    { i: '9', x: 0, y: 2, w: 1, h: 1 },
-    { i: '10', x: 1, y: 2, w: 1, h: 1 },
-    { i: '11', x: 2, y: 2, w: 1, h: 1 },
-    { i: '12', x: 3, y: 2, w: 1, h: 1 },
-    { i: '13', x: 0, y: 3, w: 1, h: 1, name: '13' },
-    { i: '14', x: 1, y: 3, w: 1, h: 1 },
-    { i: '15', x: 2, y: 3, w: 1, h: 1 },
-    { i: '16', x: 3, y: 3, w: 1, h: 1 },
-  ]
-  const rowLength = 4 // 한 행에 들어갈 요소의 갯수를 지정합니다.
-  const modifiedLayouts = layouts.map(item => {
-    const row = Math.floor(item.i / rowLength) // 요소가 위치할 행번호
-    const column = item.i % rowLength // 요소가 위치할 열번호
-    console.log(`x: ${column}, y: ${row}`)
-    return {
-      ...item,
-      x: column,
-      y: row,
-    }
-  })
+  ])
+
+  const [layouts2, setLayouts2] = useState([])
+  const [index, setIndex] = useState(1)
+  useEffect(() => {
+    console.log('레이아웃2', layouts2)
+  }, [layouts])
+
+  const onDrop = (layoutItem: any) => {
+    const updatedLayouts1 = layouts.filter(item => item.i !== layoutItem.i)
+    setLayout(updatedLayouts1)
+    console.log('빠져나가나', layouts)
+    setLayouts2([...layouts2, { ...layoutItem }])
+    setIndex(index + 1)
+  }
+  // const rowLength = 4 // 한 행에 들어갈 요소의 갯수를 지정합니다.
+  // const modifiedLayouts = layouts.map(item => {
+  //   const row = Math.floor(item.i / rowLength) // 요소가 위치할 행번호
+  //   const column = item.i % rowLength // 요소가 위치할 열번호
+  //   console.log(`x: ${column}, y: ${row}`)
+  //   return {
+  //     ...item,
+  //     x: column,
+  //     y: row,
+  //   }
+  // })
   return (
-    <div style={{ height: '100vh' }}>
-      <ResponsiveGridLayout
-        className='layout'
-        layouts={{ lg: modifiedLayouts }}
-        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        cols={{ lg: 4, md: 4, sm: 4, xs: 4, xxs: 4 }}
-        // compactType={null}
-      >
-        {modifiedLayouts.map(item => {
-          return item.name ? (
-            <Box
-              key={item.i}
-              // sx={{
-              //   display: 'flex',
-              //   alignItems: 'center',
-              //   justifyContent: 'center',
-              //   boxSizing: 'border-box',
-              // }}
-            >
-              <TextField
-                label='Name'
-                variant='outlined'
-                onChange={event => {}}
-              />
-            </Box>
-          ) : (
-            <Box
-              key={item.i}
-              sx={{
-                boxSizing: 'border-box',
-                backgroundColor: 'primary.dark',
-                '&:hover': {
-                  backgroundColor: 'primary.main',
-                  opacity: [0.9, 0.8, 0.7],
-                },
-              }}
-            >
-              {item.i}
-            </Box>
-          )
-        })}
-      </ResponsiveGridLayout>
-    </div>
+    <>
+      <div style={{ height: '50vh', background: 'black' }}>
+        <ResponsiveGridLayout
+          className='layout'
+          layouts={{ lg: layouts }}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 4, md: 4, sm: 4, xs: 4, xxs: 4 }}
+          rows={{ lg: 2, md: 2, sm: 4, xs: 4, xxs: 4 }}
+          // maxRows={4}
+          // maxCols={4}
+          compactType={null}
+          onDrop={onDrop}
+          preventCollision={true}
+        >
+          {layouts.map(item => {
+            return item.name ? (
+              <Box key={item.i}>
+                <Image
+                  src='https://source.unsplash.com/user/USERNAME'
+                  layout='fill'
+                  alt='Picture of the author'
+                />
+              </Box>
+            ) : (
+              <Box key={item.i} sx={{ background: 'white', height: '100%' }}>
+                <TextField
+                  sx={{ width: '100%', height: '100%', padding: 0 }}
+                  key={item.i}
+                  id='outlined-basic'
+                  label='Outlined'
+                  variant='outlined'
+                />
+              </Box>
+            )
+          })}
+        </ResponsiveGridLayout>
+      </div>
+      <div style={{ height: '50vh', backgroundColor: 'skyblue' }}>
+        <ResponsiveGridLayout
+          className='layout'
+          // You can define a separate onDrop function for layouts2 if needed
+          layouts={{ lg: layouts2 }}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 4, md: 4, sm: 4, xs: 4, xxs: 4 }}
+          rows={{ lg: 4, md: 4, sm: 4, xs: 4, xxs: 4 }}
+          style={{ height: '100vh' }}
+          maxRows={2}
+          malcols={2}
+          isDroppable={true}
+          preventCollision={true}
+        >
+          {layouts2.map(item => {
+            return item.name ? (
+              <Box
+                key={item.i}
+                // sx={{
+                //   display: 'flex',
+                //   alignItems: 'center',
+                //   justifyContent: 'center',
+                //   boxSizing: 'border-box',
+                // }}
+              >
+                <Image
+                  src='https://source.unsplash.com/user/USERNAME'
+                  layout='fill'
+                  alt='Picture of the author'
+                />
+              </Box>
+            ) : (
+              <Box
+                key={item.i}
+                sx={{
+                  boxSizing: 'border-box',
+                  backgroundColor: 'primary.dark',
+                  '&:hover': {
+                    backgroundColor: 'primary.main',
+                    opacity: [0.9, 0.8, 0.7],
+                  },
+                }}
+              >
+                {item.i}
+              </Box>
+            )
+          })}
+        </ResponsiveGridLayout>
+      </div>
+    </>
   )
 }
